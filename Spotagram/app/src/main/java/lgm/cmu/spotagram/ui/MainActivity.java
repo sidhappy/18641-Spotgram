336cac24ab479e5d11f9a02a4fe19b171b9f7d15
wdxtub <dacrocodilee@gmail.com>
2015-12-07 15:47:12 -0500

    search done

--- Spotagram/app/src/main/java/lgm/cmu/spotagram/ui/MainActivity.java
+++ Spotagram/app/src/main/java/lgm/cmu/spotagram/ui/MainActivity.java
@@ -1,13 +1,16 @@
 package lgm.cmu.spotagram.ui;
 
-import android.app.SearchManager;
-import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.design.widget.FloatingActionButton;
 
 import android.support.v4.view.MenuItemCompat;
@@ -17,11 +20,13 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -35,13 +40,32 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import lgm.cmu.spotagram.R;
 import lgm.cmu.spotagram.model2.Note;
 import lgm.cmu.spotagram.request.NearByRequest;
+import lgm.cmu.spotagram.utils.ConstantValue;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
@@ -49,6 +73,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private LocationManager locationManager;
     private Location location;
     private ArrayList<MarkerOptions> markers;
+    private Geocoder geoCoder;
+
+    private static final String TAG = "MAIN_ACTIVITY";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        geoCoder = new Geocoder(this, Locale.ENGLISH);
 
 
         // set location manager and location
@@ -142,29 +171,51 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
             @Override
             public boolean onQueryTextChange(String queryText) {
-                Toast.makeText(getApplicationContext(), "onQueryTextChange:" + queryText, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "onQueryTextChange:" + queryText, Toast.LENGTH_SHORT).show();
                 return true;
             }
 
             @Override
             public boolean onQueryTextSubmit(String queryText) {
-                Toast.makeText(getApplicationContext(), "onQueryTextSubmit:" + queryText, Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), "onQueryTextSubmit:" + queryText, Toast.LENGTH_SHORT).show();
+
+                String add = queryText;
+                //String add = "Times Square";
+
+                final String addressUrl = "http://maps.googleapis.com/maps/api/geocode/json?address="
+                        + URLEncoder.encode(add.replaceAll(" ", "+"))
+                        + "&sensor=true";
+
+                new GetLocationNameRequest(mMap).execute(addressUrl);
+
+
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+//得到InputMethodManager的实例
+                if (imm.isActive()) {
+//如果开启
+                    imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS);
+//关闭软键盘，开启方法相同，这个方法是切换开启与关闭状态的
+                }
+
                 return true;
             }
+
+
         });
 
         MenuItemCompat.OnActionExpandListener expandListener = new OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionCollapse(MenuItem item) {
-                Toast.makeText(getApplicationContext(), "onMenuItemActionCollapse", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "onMenuItemActionCollapse", Toast.LENGTH_SHORT).show();
                 return true;
             }
 
             @Override
             public boolean onMenuItemActionExpand(MenuItem item) {
-                Toast.makeText(getApplicationContext(), "onMenuItemActionExpand", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "onMenuItemActionExpand", Toast.LENGTH_SHORT).show();
                 return true;
             }
         };
@@ -175,12 +226,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return super.onCreateOptionsMenu(menu);
     }
 
+
+
+    public void setLoction(double lat, double lon){
+        location.setLatitude(lat);
+        location.setLatitude(lon);
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
         switch (item.getItemId()){
             case R.id.action_search:
-                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_nearby:
                 intent = new Intent(MainActivity.this, NearByActivity.class);
@@ -307,10 +366,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void setCurrentLocation(Location location){
 
         LatLng cur = new LatLng(location.getLatitude(),location.getLongitude());
-
+        this.location = location;
 
        // mMap.clear();
-        mMap.addMarker(new MarkerOptions()
+
+        Marker curMarker = mMap.addMarker(new MarkerOptions()
                 .draggable(true)
                 .position(cur)
                 .title("You are HERE"));
@@ -358,5 +418,126 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
         }
     }
+
+
+
+
+
+    public class GetLocationNameRequest extends AsyncTask<String, Void, Double[]> {
+
+        InputStream is = null;
+
+        private GoogleMap mMap;
+
+        // constructor
+        public GetLocationNameRequest(GoogleMap mMap) {
+            this.mMap = mMap;
+        }
+
+        @Override
+        protected Double[] doInBackground(String... urls) {
+
+            Double[] rtn = new Double[2];
+
+            // Making HTTP request
+            try {
+                // defaultHttpClient
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpGet httpPost = new HttpGet(urls[0]);
+
+                HttpResponse getResponse = httpClient.execute(httpPost);
+                final int statusCode = getResponse.getStatusLine().getStatusCode();
+
+                if (statusCode != HttpStatus.SC_OK) {
+                    Log.w(getClass().getSimpleName(),
+                            "Error " + statusCode + " for URL " + urls[0]);
+                    return null;
+                }
+
+                HttpEntity getResponseEntity = getResponse.getEntity();
+
+                //HttpResponse httpResponse = httpClient.execute(httpPost);
+                //HttpEntity httpEntity = httpResponse.getEntity();
+                is = getResponseEntity.getContent();
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.d("IO", e.getMessage().toString());
+                e.printStackTrace();
+
+            }
+
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(is));
+                StringBuffer sb = new StringBuffer("");
+                String line = "";
+                String NL = System.getProperty("line.separator");
+                while ((line = in.readLine()) != null) {
+                    sb.append(line + NL);
+                }
+                in.close();
+                String page = sb.toString();
+                org.json.JSONObject jsonObject = new org.json.JSONObject(page);
+                if (jsonObject.has("results")) {
+                    JSONArray jsonArray = (JSONArray) jsonObject.get("results");
+
+                    if (jsonArray.length() > 0) {
+                        jsonObject = (org.json.JSONObject) jsonArray.get(0);
+                        if (jsonObject.has("geometry")) {
+                            jsonObject = (org.json.JSONObject) jsonObject
+                                    .get("geometry");
+
+                            if (jsonObject.has("location")) {
+                                org.json.JSONObject location = (org.json.JSONObject) jsonObject
+                                        .get("location");
+
+
+                                rtn[0] = (Double) location.get("lat");
+                                rtn[1] = (Double) location.get("lng");
+
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("Buffer Error", "Error converting result " + e.toString());
+            }
+
+            // return JSON String
+            return rtn;
+
+        }
+
+        protected void onPostExecute(Double[] loc)
+        {
+            //onPostExecute
+            setLoction(loc[0],loc[1]);
+
+            LatLng cur = new LatLng(loc[0],loc[1]);
+
+            // mMap.clear();
+
+            Marker curMarker = mMap.addMarker(new MarkerOptions()
+                    .draggable(true)
+                    .position(cur)
+                    .title("You are HERE"));
+
+            // mMap.moveCamera(CameraUpdateFactory.newLatLng(cur));
+            // mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+            CameraPosition cp = new CameraPosition.Builder()
+                    .target(cur)
+                    .zoom(15)
+                    .build();
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 3000, null);
+
+        }
+
+    }
+
 
 }
